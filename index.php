602f8fdbbbf5001403c3dbda6c63cdc8b48a704f
Patrik <Cser>
2018-01-08 16:44:13 +0100

    Szín és szöveg változtatás.

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 
-$nev = 'HGabor';
+$nev = 'omayva mo shindeiru';
 $datum = new \DateTime();
 $datumStr = $datum->format('Y-m-d\TH:i:s');
-$szin = '#ff0000';
+$szin = '#16ce6f';
 
 require 'contents.php';
